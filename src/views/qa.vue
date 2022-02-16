61f6e00e042bde47c90561fbacffd06a54468b78
nokranger <anusorn.thavornporn@gmail.com>
2022-02-16 14:29:43 +0700

    qa

--- src/views/qa.vue
+++ src/views/qa.vue
@@ -84,8 +84,8 @@
       <div style="background-color: #f7f7f7">
         <b-container>
           <div>
-            <br>
-            <br>
+            <br />
+            <br />
             <img
               src="https://tyrepac.co.th/wp-content/uploads/2019/12/icon-3lines.png"
               alt=""
@@ -118,12 +118,255 @@
             </b-col>
             <b-col>
               <div>
-                <img src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tyre-guide.jpg" alt="">
+                <img
+                  src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tyre-guide.jpg"
+                  alt=""
+                />
               </div>
             </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-col>
+                <br />
+                <div style="background-color: white">
+                  <div>Directional ดอกยางแบบทิศทางเดียว</div>
+                  <br />
+                  <br />
+                  <div>รายละเอียด</div>
+                  <p>
+                    ลวดลายดอกยางถูกออกแบบมาเพื่อหมุนไปในทิศทางเดียวเท่านั้น
+                    ด้วยเหตุผลนี้ยางจึงมีสัญลักษณ์ลูกศร แสดงอยู่ที่ขอบยาง
+                    เพื่อระบุทิศทางในการติดตั้งยาง
+                    ลายร่องด้านข้างทั้งสองข้างของยาง
+                    จะหันไปทางกึ่งกลางของยางทำให้เกิดรูปร่างตัววี (v)
+                    ร่องยางลักษณะนี้สามารถระบายน้ำได้อย่างมีประสิทธิภาพ
+                    ผ่านดอกยาง เพื่อให้ยางสามารถสัมผัสกับถนน
+                    และเพื่อเพิ่มการยึดเกาะถนนเปียก
+                    และยังสามารถต้านทานแรงดันน้ำได้แม้ในความเร็วสูง
+                  </p>
+                  <br />
+                  <div>ข้อดี</div>
+                  <p>
+                    เหมาะกับรถยนต์สมรรถนะสูง
+                    จะให้ความเสถียรเมื่อขับขี่ด้วยความเร็วสูง
+                  </p>
+                  <div>
+                    <img
+                      src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tyre-directional.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </b-col>
+            </b-col>
+            <b-col>
+              <b-col>
+                <br />
+                <div style="background-color: white">
+                  <div>Symmetrical Design ดอกยางแบบสมมาตร</div>
+                  <br />
+                  <br />
+                  <div>รายละเอียด</div>
+                  <p>
+                    เป็นรูปแบบดอกยางที่พบมากที่สุด
+                    สำหรับรถซีดานมาตรฐานและรถบรรทุกขนาดเล็ก
+                    ยางเหล่านี้มีบล็อกดอกยางแบบต่อเนื่องทั่วพื้นผิวทั้งหมดของยาง
+                    ลวดลายในแต่ละด้านของยางนั้นเหมือนกันทุกประการ
+                  </p>
+                  <br />
+                  <div>ข้อดี</div>
+                  <p>
+                    ดอกยางเป็นลายเล็ก ๆ และละเอียด
+                    ช่วยลดเสียงของยางและเสียงเมื่อสัมผัสกับถนน
+                    ทำให้เป็นยางที่ค่อนข้างเงียบ
+                  </p>
+                  <p>
+                    การสึกหรอและอายุการใช้งานที่ยาวนาน
+                    เนื่องจากสามารถสลับยางได้ทั้ง 4 เส้น
+                    เพื่อให้การสึกหรอเท่ากันในทุกๆ ด้าน
+                  </p>
+                  <div>
+                    <img
+                      src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tyre-directional.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </b-col>
+            </b-col>
+            <b-col>
+              <b-col>
+                <br />
+                <div style="background-color: white">
+                  <div>Asymmetric Design ดอกยางแบบอสมมาตร</div>
+                  <br />
+                  <br />
+                  <div>รายละเอียด</div>
+                  <p>
+                    ลักษณะลายดอกยางบนหน้ายางสองฝั่งไม่เหมือนกัน
+                    เพื่อช่วยเพิ่มประสิทธิภาพในการขับขี่ให้ดียิ่งขึ้น
+                    หน้ายางด้านในเหมาะสำหรับขับทางตรงและใช้ความเร็วสูง
+                    ในขณะที่หน้ายางด้านนอกมีหน้าที่ช่วยยึดเกาะถนนได้ดีเวลาเข้าโค้ง
+                    เหมาะกับการขับรถที่ใช้ความเร็วและต้องการเข้าโค้งที่ปลอดภัย
+                  </p>
+                  <br />
+                  <div>ข้อดี</div>
+                  <p>
+                    เหมาะกับรถยนต์สมรรถนะสูง และรถยนต์ที่ใช้ความเร็ว
+                    เพื่อการทรงตัวที่ดีขณะเข้าโค้ง
+                  </p>
+                  <p>
+                    สามารถสลับยางได้ทั้ง 4 ล้อ
+                    เพื่อให้การสึกหรอเป็นไปอย่างสม่ำเสมอ และช่วยยืดอายุการใช้งาน
+                    โดยบนแก้มยางแบบ “อสมมาตร” จะมีคำว่า “inside” และ “outside”
+                    อยู่บนแก้มยาง เมื่อต้องสลับยาง
+                    ต้องดูให้แน่ใจว่าได้เอาด้านที่มีคำว่า “outside”
+                    ไว้ด้านนอกเสมอ
+                  </p>
+                  <div>
+                    <img
+                      src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tyre-directional.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </b-col>
+            </b-col>
+          </b-row>
+          <b-row>
+            <div>B.MIXED TYRE</div>
+            <b-col>
+              <div>
+                <div>สามารถเปลี่ยนยางเส้นเดียวได้หรือไม่ ?</div>
+                <p>
+                  สามารถเปลี่ยนได้แต่อย่างไรก็ตามเราไม่แนะนำให้เปลี่ยนยางที่แตกต่างกันทีละเส้น
+                  ควรเปลี่ยนยางเป็นคู่เพื่อป้องกันการสึกหรอหรือความไม่สมดุลของยาง
+                </p>
+              </div>
+            </b-col>
+            <b-col>
+              <div>
+                <div>ควรติดตั้งยางคู่ใหม่ที่ไหน?</div>
+                <p>
+                  ติดที่เพลาล้อหลัง
+                  การยึดเกาะถนนกับเพลาล้อหลังบนพื้นผิวที่เปียกนั้นเป็นสิ่งจำเป็นเพื่อป้องกันไม่ให้เกิดสภาวะการเลี้ยวเกินและสูญเสียการควบคุมรถโดยเฉพาะอย่างยิ่งในระหว่างการเบรคอย่างกะทันหัน
+                </p>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <div>C.ข้อมูลทางเทคนิค</div>
+            <div>ข้อมูลยางพื้นฐานที่ควรทราบ</div>
+            <div style="background-color: white">
+              <b-row>
+                <b-col>
+                  <div>
+                    <img
+                      src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/basic_tyreinfo.png"
+                      alt=""
+                    />
+                  </div>
+                </b-col>
+                <b-col>
+                  <br>
+                  <br>
+                  <div v-for="(details, index) in detail" :key="index">
+                    <div>{{index + 1}}. {{details.name}}</div>
+                  </div>
+                  <div>**(C & U regulations stipulate all tyres used in the UK must carry an E number)</div>
+                </b-col>
+              </b-row>
+            </div>
+          </b-row>
+          <br>
+          <br>
+          <b-row>
+            <b-col>
+              <div style="background-color: white;">
+                <div>
+                  ขนาดยาง
+                </div>
+                <p>รหัสยางซึ่งขึ้นรูปอยู่กับแก้มยางโดยทั่วไป จะอธิบายด้วยค่าตัวเลขและตัวอักษร รหัสนี้ระบุขนาดของยางและข้อจำกัดที่สำคัญบางประการ เช่น ความสามารถในการรับน้ำหนักและความเร็วสูงสุด</p>
+                <div style="margin: 10px;">
+                  <img src="https://www.tyrepac.co.th/wp-content/uploads/2019/12/tech-tyresize.jpg" alt="">
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div style="background-color: white;">
+                <div>สัญลักษณ์ความเร็ว
+              </div>
+              <p>An alphabet representing the maximum speed of this tyre.</p>
+                <div style="margin: 10px;">
+                  <b-table striped hover :items="items"></b-table>
+                </div>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <div style="background-color: white;">
+              <div>ดัชนีการรับน้ำหนัก</div>
+              <p>A numeric code that tells you the maximum load carrying ability of each tyre.</p>
+              <div style="margin: 10px;">Table</div>
+            </div>
           </b-row>
         </b-container>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      detail: [{
+        name: 'Tyre brand name'
+      }, {
+        name: 'Pattern or model code'
+      }, {
+        name: 'Tyre size'
+      }, {
+        name: 'Service description (Load index & Speed symbol)'
+      }, {
+        name: 'ECE or EEC type approval number**'
+      }, {
+        name: 'ECE noise approval'
+      }, {
+        name: 'North American recall code'
+      }, {
+        name: 'Date of manufacture code'
+      }, {
+        name: 'UTQG quality rating'
+      }, {
+        name: 'Maximum permitted load (USA)'
+      }, {
+        name: 'Maximum permitted inflation pressure (USA)'
+      }, {
+        name: 'Denotes tubeless construction'
+      }, {
+        name: 'Safety warning'
+      }, {
+        name: 'Direction of rotation (Directional or Composite tyres)'
+      }, {
+        name: 'Rim fitment instruction (Asymmetrical or Composite tyres)'
+      }, {
+        name: 'Denotes extra load tyre construction'
+      }, {
+        name: 'Denotes reinforced tyre construction'
+      }, {
+        name: 'Position of tread wear indicators.'
+      }],
+      items: [
+        { speed: 'S', Category: '180 km/h ( 112 mph )' },
+        { speed: 'T', Category: '190 km/h ( 118 mph )' },
+        { speed: 'U', Category: '200 km/h ( 124 mph )' },
+        { speed: 'H', Category: '210 km/h ( 130 mph )' },
+        { speed: 'V', Category: '240 km/h ( 149 mph )' },
+        { speed: '*W', Category: '270 km/h ( 168 mph )' },
+        { speed: '*Y', Category: '300 km/h ( 186 mph )' }
+      ]
+    }
+  }
+}
+</script>
