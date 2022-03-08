c7781d8694a59724b6a9fae8fcd86412608621eb
nokranger <anusorn.thavornporn@gmail.com>
2022-03-08 13:16:10 +0700

    multi range slider

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -89,18 +89,22 @@
                     </div>
                     <div>
                       <label style="margin-left: 5px;" for="range-2">กรองตามราคา</label>
-                      <b-form-input
-                        id="range-2"
-                        v-model="value"
-                        type="range"
-                        min="2250"
-                        max="21100"
-                        step="10"
-                      ></b-form-input>
-                      <div style="margin-left: 5px;" class="mt-2">ราคา {{ 2250 }} - {{ value }}</div>
-                      <b-button variant="primary" v-on:click="filterprice(value)">กรอง</b-button>
+                      <div class='range-slider' style="margin-top: -60px;width: 100%;">
+                        <input type="range" min="2250" max="21100" step="1" v-model="sliderMin">
+                        <input type="range" min="2250" max="21100" step="1" v-model="sliderMax">
+                      </div>
+                      <div style="margin-left: 5px;" class="mt-2">ราคา {{ sliderMin }} - {{ sliderMax }}</div>
+                      <b-button variant="primary" v-on:click="filterprice(sliderMin, sliderMax)">กรอง</b-button>
                       <br />
                     </div>
+                    <!-- <div>
+                      <div class='range-slider'>
+                        <input type="range" min="0" max="180" step="1" v-model="sliderMin">
+                        <input type="number" min="0" max="180" step="1" v-model="sliderMin">
+                        <input type="range" min="0" max="180" step="1" v-model="sliderMax">
+                        <input type="number" min="0" max="180" step="1" v-model="sliderMax">
+                      </div>
+                    </div> -->
                   </div>
                 </div>
                 <br />
@@ -264,7 +268,9 @@ export default {
       promotions: [],
       checkcart: [],
       checkcarts: [],
-      checkcartz: []
+      checkcartz: [],
+      minAngle: 2250,
+      maxAngle: 21100
     }
   },
   async mounted () {
@@ -479,12 +485,12 @@ export default {
         this.brand = res.data.data.products
       })
     },
-    filterprice (value) {
-      console.log('valuefilterswidth', value)
+    filterprice (min, max) {
+      console.log('valuefilterswidth', min + max)
       var data = JSON.stringify({
         // eslint-disable-next-line quote-props
-        minPrice: 2250,
-        maxPrice: value
+        minPrice: min,
+        maxPrice: max
       })
       var config = {
         method: 'post',
@@ -531,166 +537,7 @@ export default {
         }
         localStorage.setItem('cart', JSON.stringify(cart))
         location.replace('/checkout')
-        // this.checkcart = []
-        // this.checkcart = JSON.parse(localStorage.getItem('cart'))
-        // if (Array.isArray(this.checkcart)) {
-        //   console.log('is array')
-        //   this.checkcart = JSON.parse(localStorage.getItem('cart'))
-        //   this.checkcartz = this.checkcart.find((post, index) => {
-        //     post.value = post.value + this.$refs[value][0].localValue
-        //     return true
-        //   })
-        // } else {
-        //   console.log('not array')
-        //   this.checkcart = [JSON.parse(localStorage.getItem('cart'))]
-        //   this.checkcartz = this.checkcart.find((post, index) => {
-        //     post.value = post.value + this.$refs[value][0].localValue
-        //     return true
-        //   })
-        //   console.log('count', this.checkcartz)
-        // }
-        // console.log('checkcart', this.checkcart)
-        // localStorage.setItem('cart', JSON.stringify(this.checkcart))
-        // location.replace('/checkout')
-        // Object.keys(this.checkcart).forEach(key => this.checkcart.push())
-        // eslint-disable-next-line no-unused-vars
-        // const checkcarts = this.checkcart.find((post, index) => {
-        //   if (post.name === name) {
-        //     post.value = post.value + this.$refs[value][0].localValue
-        //     console.log('namesum', post.value)
-        //     const cart = {
-        //       url: url,
-        //       img: img,
-        //       name: name,
-        //       price: price,
-        //       value: post.value,
-        //       type: type,
-        //       sku: sku,
-        //       brand: brand
-        //     }
-        //     localStorage.setItem('cart', JSON.stringify(cart))
-        //     console.log('add value', checkcarts)
-        //     // location.replace('/cart')
-        //   } else if (post.name !== name) {
-        //     console.log('type', post.name)
-        //     console.log('typename', name)
-        //     console.log('array', Array.isArray(this.checkcart))
-        //     // this.checkcart = [JSON.parse(localStorage.getItem('cart'))]
-        //     // if (Array.isArray(this.checkcart)) {
-        //     //   console.log('more')
-        //     //   this.checkcart = JSON.parse(localStorage.getItem('cart'))
-        //     // } else {
-        //     //   console.log('not array')
-        //     // }
-        //     // console.log('new tyre', this.checkcart)
-        //     // const cart = {
-        //     //   url: url,
-        //     //   img: img,
-        //     //   name: name,
-        //     //   price: price,
-        //     //   value: this.$refs[value][0].localValue,
-        //     //   type: type,
-        //     //   sku: sku,
-        //     //   brand: brand
-        //     // }
-        //     // // this.checkcart = Object.assign(this.checkcart, cart)
-        //     // console.log('new cartsss', this.checkcart)
-        //     // // this.checkcart.push(cart)
-        //     // this.checkcart.push(cart)
-        //     // console.log('add cart', this.checkcart)
-        //     // localStorage.setItem('cart', JSON.stringify(this.checkcart))
-        //   }
-        //   // console.log('new tyre', this.checkcart)
-        //   // const cart = {
-        //   //   url: url,
-        //   //   img: img,
-        //   //   name: name,
-        //   //   price: price,
-        //   //   value: this.$refs[value][0].localValue,
-        //   //   type: type,
-        //   //   sku: sku,
-        //   //   brand: brand
-        //   // }
-        //   // // this.checkcart = Object.assign(this.checkcart, cart)
-        //   // console.log('new cartsss', this.checkcart)
-        //   // // this.checkcart.push(cart)
-        //   // this.checkcart.push(cart)
-        //   // console.log('add cart', this.checkcart)
-        //   // localStorage.setItem('cart', JSON.stringify(this.checkcart))
-        //   // this.checkcart = checkcarts
-        //   // console.log('checkcart', this.checkcart)
-        // })
-      //   this.checkcart = JSON.parse(localStorage.getItem('cart'))
-      //   this.checkcart.find((post, index) => {
-      //     if (post.name !== name) {
-      //       // post.value = post.value + this.$refs[value][0].localValue
-      //       console.log('type', post.name)
-      //       console.log('typename', name)
-      //       const cart = {
-      //         url: url,
-      //         img: img,
-      //         name: name,
-      //         price: price,
-      //         value: post.value,
-      //         type: type,
-      //         sku: sku,
-      //         brand: brand
-      //       }
-      //       this.checkcart.push(cart)
-      //       // localStorage.setItem('cart', this.checkcart)
-      //       console.log('add value', this.checkcart)
-      //       // location.replace('/cart')
-      //     } else {
-      //       console.log('type', post.name)
-      //       console.log('typename', name)
-      //       console.log('array', Array.isArray(this.checkcart))
-      //       // this.checkcart = [JSON.parse(localStorage.getItem('cart'))]
-      //       // if (Array.isArray(this.checkcart)) {
-      //       //   console.log('more')
-      //       //   this.checkcart = JSON.parse(localStorage.getItem('cart'))
-      //       // } else {
-      //       //   console.log('not array')
-      //       // }
-      //       // console.log('new tyre', this.checkcart)
-      //       // const cart = {
-      //       //   url: url,
-      //       //   img: img,
-      //       //   name: name,
-      //       //   price: price,
-      //       //   value: this.$refs[value][0].localValue,
-      //       //   type: type,
-      //       //   sku: sku,
-      //       //   brand: brand
-      //       // }
-      //       // // this.checkcart = Object.assign(this.checkcart, cart)
-      //       // console.log('new cartsss', this.checkcart)
-      //       // // this.checkcart.push(cart)
-      //       // this.checkcart.push(cart)
-      //       // console.log('add cart', this.checkcart)
-      //       // localStorage.setItem('cart', JSON.stringify(this.checkcart))
-      //     }
-      //     // console.log('new tyre', this.checkcart)
-      //     // const cart = {
-      //     //   url: url,
-      //     //   img: img,
-      //     //   name: name,
-      //     //   price: price,
-      //     //   value: this.$refs[value][0].localValue,
-      //     //   type: type,
-      //     //   sku: sku,
-      //     //   brand: brand
-      //     // }
-      //     // // this.checkcart = Object.assign(this.checkcart, cart)
-      //     // console.log('new cartsss', this.checkcart)
-      //     // // this.checkcart.push(cart)
-      //     // this.checkcart.push(cart)
-      //     // console.log('add cart', this.checkcart)
-      //     // localStorage.setItem('cart', JSON.stringify(this.checkcart))
-      //     // this.checkcart = checkcarts
-      //     // console.log('checkcart', this.checkcart)
-      //   })
       }
-      // console.log('buy', url + '-' + img + '-' + name + '-' + price + '-' + this.$refs[value][0].localValue)
     },
     buydetail (url, img, name, price, value, type, sku, brand) {
       console.log('buy', url + '-' + img + '-' + name + '-' + price + '-' + this.$refs[value][0].localValue)
@@ -707,6 +554,113 @@ export default {
       localStorage.setItem('cartdetail', JSON.stringify(cart))
       location.replace('/tyrebrand/?brand=' + sku)
     }
+  },
+  computed: {
+    sliderMin: {
+      get: function () {
+        var val = parseInt(this.minAngle)
+        return val
+      },
+      set: function (val) {
+        val = parseInt(val)
+        if (val > this.maxAngle) {
+          this.maxAngle = val
+        }
+        this.minAngle = val
+      }
+    },
+    sliderMax: {
+      get: function () {
+        var val = parseInt(this.maxAngle)
+        return val
+      },
+      set: function (val) {
+        val = parseInt(val)
+        if (val < this.minAngle) {
+          this.minAngle = val
+        }
+        this.maxAngle = val
+      }
+    }
   }
 }
 </script>
+<style scoped>
+.range-slider {
+  width: 300px;
+  margin: auto;
+  text-align: center;
+  position: relative;
+  height: 6em;
+}
+
+.range-slider input[type=range] {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+
+input[type=number] {
+  border: 1px solid #ddd;
+  text-align: center;
+  font-size: 1.6em;
+  -moz-appearance: textfield;
+}
+
+input[type=number]::-webkit-outer-spin-button,
+input[type=number]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+input[type=number]:invalid,
+input[type=number]:out-of-range {
+  border: 2px solid #ff6347;
+}
+
+input[type=range] {
+  -webkit-appearance: none;
+  width: 100%;
+}
+
+input[type=range]:focus {
+  outline: none;
+}
+
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: #2497e3;
+}
+
+input[type=range]:focus::-ms-fill-lower {
+  background: #2497e3;
+}
+
+input[type=range]:focus::-ms-fill-upper {
+  background: #2497e3;
+}
+
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 5px;
+  cursor: pointer;
+  animation: 0.2s;
+  background: #2497e3;
+  border-radius: 1px;
+  box-shadow: none;
+  border: 0;
+}
+
+input[type=range]::-webkit-slider-thumb {
+  z-index: 2;
+  position: relative;
+  box-shadow: 0px 0px 0px #000;
+  border: 1px solid #2497e3;
+  height: 18px;
+  width: 18px;
+  border-radius: 25px;
+  background: #a1d0ff;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -7px;
+}
+
+</style>
