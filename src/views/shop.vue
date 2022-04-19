708f612e00ac9aaa521efa7f750d8126d84e2b84
nokranger <anusorn.thavornporn@gmail.com>
2022-04-19 17:32:46 +0700

    ref

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -196,7 +196,7 @@
                       <div style="border-radius: 5px;border: thin solid #E0E0E0;text-align: left;height: 420px;">
                         <div style="width: 100%;">
                           <a style="cursor: pointer;" v-on:click="buydetail (('/tyrebrand' + '/' + brandd.prodId + '/' + brandd.sku), ('https://www.tyrepac.co.th/wp-content/uploads/2020/02/tyre-toyo-proxes-sport-suv.jpg'), brandd.name, brandd.regularPrice, ('values' + index), brandd.type, brandd.sku, brandd)">
-                            <img src="https://www.tyrepac.co.th/wp-content/uploads/2020/02/tyre-toyo-proxes-sport-suv.jpg" style="width: 100%;height: auto;margin: 5px;" alt="">
+                            <img src="https://www.tyrepac.co.th/wp-content/uploads/2020/02/tyre-toyo-proxes-sport-suv.jpg" style="width: 100%;height: auto;margin: 5px;" alt="" loading="lazy">
                           </a>
                         </div>
                         <div style="margin-left: 5px;color: #005099;font-weight: bold;height: 70px;">{{brandd.name}}</div>
