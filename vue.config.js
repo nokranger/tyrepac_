baa33874d366aa51d426aea7393a49415d4b4a9a
nokranger <anusorn.thavornporn@gmail.com>
2022-05-30 17:07:40 +0700

    ref

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-// module.exports = {
+module.exports = {
 //   devServer: {
 //     proxy: process.env.VUE_APP_API_URL
 //   },
-//   // devServer: {
-//   //   proxy: 'http://192.168.10.182:4000'
-//   // },
-//   productionSourceMap: false
-// }
+  // devServer: {
+  //   proxy: 'http://192.168.10.182:4000'
+  // },
+  productionSourceMap: false
+}
