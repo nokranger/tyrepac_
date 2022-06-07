d48cb5f373b925e08a34c81bbeaf29ffd5dfd827
nokranger <anusorn.thavornporn@gmail.com>
2022-06-07 16:35:52 +0700

    date format

--- src/views/checkoutcredit.vue
+++ src/views/checkoutcredit.vue
@@ -110,7 +110,7 @@
             </div>
             <ul>
               <div>
-                <div style="display: inline-block;font-weight: bold;">วันที่: </div><li>{{dates | moment('dddd, MMMM Do YYYY')}}</li>
+                <div style="display: inline-block;font-weight: bold;">วันที่: </div><li>{{dates | moment('DD/MM/YYYY')}}</li>
               </div>
               <div>
                 <!-- <div style="display: inline-block;font-weight: bold;">เลขที่คำสั่งซื้อ: </div><li>-</li> -->
